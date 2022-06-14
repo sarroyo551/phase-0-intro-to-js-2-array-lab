648fa82389978589ce4992755386c314b46353ef
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-14 10:37:04 -0400

    commit tree 3aef3eaf2442edd07e940848edb7a70f95b87606

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat (names) {
+  cats.push(names);
+}
+
+function destructivelyPrependCat (names) {
+  cats.unshift(names);
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}    
+
+function appendCat(name) {
+    let newCats = [...cats, name]
+    return newCats;
+}
+
+function prependCat(name) {
+    let newCats = [...cats]
+    newCats.unshift(name);
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = [...cats]
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat () {
+    let newCats = [...cats]
+    newCats.shift();
+    return newCats;
+}
